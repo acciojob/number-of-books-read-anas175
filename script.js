e61fe36fb2961d0fa2c2139a577f1094bfe3fbd8
Anas Adnan <anasadnan175@gmail.com>
2023-06-15 13:29:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	var c = 0;
+for(let i=0; i<library.length; i++){
+if( library[i].readingStatus === true){
+c++;
+}
+
+}
+return c;
 };
 
 // Do not change the code below
